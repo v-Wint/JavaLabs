16d42a39dbd024fbfd70e8f465e4e7c58cc31691
v-Wint <tuiooad@gmail.com>
2023-10-22 22:05:18 +0300

    Lab 1 done

---
+++ src/lab1/Main.java
@@ -0,0 +1,38 @@
+package lab1;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        1222
+        O1: +
+        C:  1
+        O2: %
+        index type = char
+
+        sum i in {a, n} (sum j in {b, m} ((i % j) / (i + C)))
+         */
+
+        final int C = 1;
+
+        char a = 1;
+        char n = 7;
+        char b = 2;
+        char m = 5;
+
+        // if b == 0, j == 0, i % j div by zero
+        if (b == 0) {
+            System.out.println("Division by zero");
+            return;
+        }
+
+        double sum = 0;
+
+        for (char i = a; i <= n; i++) {
+            for (char j = b; j <= m; j++) {
+                sum += (double)i % j / (i + C);
+            }
+        }
+
+        System.out.printf("Result: %.4f", sum);
+    }
+}
